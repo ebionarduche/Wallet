cd9a59c38e224ca9c43e3c15257755b2e92353e6
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-18 10:30:44 -0300

    css table.css - Acerto da direção da table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,66 +8,67 @@ class Table extends Component {
   render() {
     const { expenses, dispatch } = this.props;
     return (
-      <table>
-        <thead>
-          <tr>
-            <th className="table">Descrição</th>
-            <th className="table">Tag</th>
-            <th className="table">Método de pagamento</th>
-            <th className="table">Valor</th>
-            <th className="table">Moeda</th>
-            <th className="table">Câmbio utilizado</th>
-            <th className="table">Moeda de conversão</th>
-            <th className="table">Valor convertido</th>
-            <th className="table">Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            expenses.map(({
-              description,
-              id,
-              tag,
-              method,
-              value,
-              exchangeRates,
-              currency,
-            }) => (
-              <tr key={ id }>
-                <td>{description}</td>
-                <td>{tag}</td>
-                <td>{method}</td>
-                <td>{Number(value).toFixed(2)}</td>
-                <td>{exchangeRates[currency].name}</td>
-                <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
-                <td>Real</td>
-                <td>{Number(exchangeRates[currency].ask * value).toFixed(2)}</td>
-                <td>
-                  <div className="table-btn">
-                    <button
-                      type="button"
-                      className="edit-btn"
-                      data-testid="edit-btn"
-                      onClick={ () => dispatch(editExpenseInit(id)) }
-                    >
-                      Editar
-                    </button>
-
-                    <button
-                      type="button"
-                      className="remove-btn"
-                      data-testid="delete-btn"
-                      onClick={ () => dispatch(removeExpense(id)) }
-                    >
-                      Excluir
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <div className="table-main">
+        <table>
+          <thead>
+            <tr>
+              <th className="table">Descrição</th>
+              <th className="table">Tag</th>
+              <th className="table">Método de pagamento</th>
+              <th className="table">Valor</th>
+              <th className="table">Moeda</th>
+              <th className="table">Câmbio utilizado</th>
+              <th className="table">Moeda de conversão</th>
+              <th className="table">Valor convertido</th>
+              <th className="table">Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              expenses.map(({
+                description,
+                id,
+                tag,
+                method,
+                value,
+                exchangeRates,
+                currency,
+              }) => (
+                <tr key={ id }>
+                  <td>{description}</td>
+                  <td>{tag}</td>
+                  <td>{method}</td>
+                  <td>{Number(value).toFixed(2)}</td>
+                  <td>{exchangeRates[currency].name}</td>
+                  <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                  <td>Real</td>
+                  <td>{Number(exchangeRates[currency].ask * value).toFixed(2)}</td>
+                  <td>
+                    <div className="table-btn">
+                      <button
+                        type="button"
+                        className="edit-btn"
+                        data-testid="edit-btn"
+                        onClick={ () => dispatch(editExpenseInit(id)) }
+                      >
+                        Editar
+                      </button>
+                      <button
+                        type="button"
+                        className="remove-btn"
+                        data-testid="delete-btn"
+                        onClick={ () => dispatch(removeExpense(id)) }
+                      >
+                        Excluir
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -59,105 +59,102 @@ class WalletForm extends Component {
     const { currencies, editor, dispatch } = this.props;
     const { value, description, currency, method, tag } = this.state;
     return (
-      <form>
-        <label htmlFor="">
-          Valor:
-          <input
-            type="number"
-            name="value"
-            value={ value }
-            id=""
-            data-testid="value-input"
-            onChange={ this.onInputChange }
-          />
-        </label>
-
-        <label htmlFor="">
-          Descrição:
-          <input
-            type="text"
-            name="description"
-            value={ description }
-            id=""
-            data-testid="description-input"
-            onChange={ this.onInputChange }
-          />
-        </label>
-
-        <label htmlFor="">
-          Moeda
-          <select
-            name="currency"
-            value={ currency }
-            id=""
-            data-testid="currency-input"
-            onChange={ this.onInputChange }
-          >
-            {
-              currencies.map((moeda, index) => (
-                <option key={ index } value={ moeda }>{ moeda }</option>
-              ))
-            }
-          </select>
-        </label>
-
-        <label htmlFor="">
-          Forma de Pagamento:
-          <select
-            className="form-options"
-            name="method"
-            value={ method }
-            id=""
-            data-testid="method-input"
-            onChange={ this.onInputChange }
-          >
-            <option value="Dinheiro">Dinheiro</option>
-            <option value="Cartão de crédito">Cartão de crédito</option>
-            <option value="Cartão de débito">Cartão de débito</option>
-          </select>
-        </label>
-
-        <label htmlFor="">
-          Categoria:
-          <select
-            className="form-options"
-            name="tag"
-            value={ tag }
-            id=""
-            data-testid="tag-input"
-            onChange={ this.onInputChange }
-          >
-            <option value="Alimentação">Alimentação</option>
-            <option value="Lazer">Lazer</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Transporte">Transporte</option>
-            <option value="Saúde">Saúde</option>
-          </select>
-        </label>
-        {
-          editor ? (
-            <button
-              type="button"
-              onClick={ this.expenseEditSubmit }
+      <div>
+        <form>
+          <label htmlFor="">
+            Valor:
+            <input
+              type="number"
+              name="value"
+              value={ value }
+              id=""
+              data-testid="value-input"
+              onChange={ this.onInputChange }
+            />
+          </label>
+          <label htmlFor="">
+            Descrição:
+            <input
+              type="text"
+              name="description"
+              value={ description }
+              id=""
+              data-testid="description-input"
+              onChange={ this.onInputChange }
+            />
+          </label>
+          <label htmlFor="">
+            Moeda
+            <select
+              name="currency"
+              value={ currency }
+              id=""
+              data-testid="currency-input"
+              onChange={ this.onInputChange }
             >
-              Editar despesa
-            </button>
-          )
-            : (
+              {
+                currencies.map((moeda, index) => (
+                  <option key={ index } value={ moeda }>{ moeda }</option>
+                ))
+              }
+            </select>
+          </label>
+          <label htmlFor="">
+            Forma de Pagamento:
+            <select
+              className="form-options"
+              name="method"
+              value={ method }
+              id=""
+              data-testid="method-input"
+              onChange={ this.onInputChange }
+            >
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="">
+            Categoria:
+            <select
+              className="form-options"
+              name="tag"
+              value={ tag }
+              id=""
+              data-testid="tag-input"
+              onChange={ this.onInputChange }
+            >
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+          {
+            editor ? (
               <button
                 type="button"
-                onClick={ (event) => {
-                  event.preventDefault();
-                  dispatch(requestAndAddExpense(this.state));
-                  this.resetFormState();
-                } }
+                onClick={ this.expenseEditSubmit }
               >
-
-                Adicionar despesa
+                Editar despesa
               </button>
             )
-        }
-      </form>
+              : (
+                <button
+                  type="button"
+                  onClick={ (event) => {
+                    event.preventDefault();
+                    dispatch(requestAndAddExpense(this.state));
+                    this.resetFormState();
+                  } }
+                >
+                  Adicionar despesa
+                </button>
+              )
+          }
+        </form>
+      </div>
     );
   }
 }
